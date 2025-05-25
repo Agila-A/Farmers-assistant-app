ec6273ff557f67408c9486193bf497aa81862216
Agila A <a.agila2005@gmail.com>
2025-05-25 22:38:07 +0530

    Altered changes - Agila A
    Temp commit before applying stash

--- src/Pages/CalendarPage.jsx
+++ src/Pages/CalendarPage.jsx
@@ -1,11 +1,31 @@
-import React from 'react';
-import Calendar from '../Components/Calendar';
+// src/pages/CalendarPage.jsx
+import React, { useState } from 'react';
+import Calendar from '../components/Calendar';
 
 const CalendarPage = () => {
+  const [events, setEvents] = useState([]);
+  const [newEvent, setNewEvent] = useState({ date: '', task: '' });
+
+  const handleAddEvent = () => {
+    if (newEvent.date && newEvent.task) {
+      setEvents([...events, newEvent]);
+      setNewEvent({ date: '', task: '' });
+    }
+  };
+
+  const handleDeleteEvent = (index) => {
+    const updatedEvents = events.filter((_, i) => i !== index);
+    setEvents(updatedEvents);
+  };
+
   return (
-    <div>
-      <Calendar />
-    </div>
+    <Calendar
+      events={events}
+      newEvent={newEvent}
+      setNewEvent={setNewEvent}
+      handleAddEvent={handleAddEvent}
+      handleDeleteEvent={handleDeleteEvent}
+    />
   );
 };
 
