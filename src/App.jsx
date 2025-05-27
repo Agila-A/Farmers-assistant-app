f06380134038719404904c473f0e0522aa823114
Agila A <a.agila2005@gmail.com>
2025-05-27 11:35:19 +0530

    Community feature - Agila

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-
+import CommunityPage from './Pages/CommunityPage';
 import CalendarPage from './Pages/CalendarPage';
 import LandingPage from './Pages/LandingPage';
 import SignUpPage from './Pages/SignUpPage';
@@ -20,7 +20,9 @@ const App = () => {
         <Route path="/login" element={<LoginPage />} />
 
         {/* Pages with sidebar */}
-        <Route path="/dashboard" element={<Layout><Dashboard /></Layout>} /> {/* ✅ Added */}
+        <Route path="/dashboard" element={<Layout><Dashboard /></Layout>} /> 
+        <Route path="/community" element={<Layout><CommunityPage /></Layout>} />
+        {/* ✅ Added */}
         <Route path="/agrilend" element={<Layout><AgriLendPage /></Layout>} />
         <Route path="/schedule" element={<Layout><CalendarPage /></Layout>} />
       </Routes>
