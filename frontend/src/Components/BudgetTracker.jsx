61cfd09f66db26f2fb623a4ec402e93aac590879
Abinaya S <abisekar1211@gmail.com>
2025-05-28 10:38:12 +0530

    Update BudgetTracker component

--- frontend/src/Components/BudgetTracker.jsx
+++ frontend/src/Components/BudgetTracker.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
-import '../styles/BudgetTracker.css'; 
+import '../Styles/BudgetTracker.css'; 
 
 const BudgetTracker = () => {
   const navigate = useNavigate();
