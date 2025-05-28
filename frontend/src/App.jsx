0f5d9fbec44d1ac231253107ffbf6c8d6c2e4121
Abinaya S <abisekar1211@gmail.com>
2025-05-28 11:30:15 +0530

    update project files

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,7 +7,6 @@ import SignUpPage from './Pages/SignUpPage';
 import LoginPage from './Pages/LoginPage';
 import AgriLendPage from './Pages/AgriLendPage';
 import Dashboard from './Components/Dashboard'; // ✅ Add this
-import BudgetTracker from './Pages/BudgetTracker';
 import Layout from './Components/Layout';
 
 const App = () => {
