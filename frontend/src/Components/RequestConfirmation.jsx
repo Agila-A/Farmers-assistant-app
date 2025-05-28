192e6d43065a2a84c74a196f4580d3070445a18a
Agila A <a.agila2005@gmail.com>
2025-05-28 19:01:28 +0530

    done

--- frontend/src/Components/RequestConfirmation.jsx
+++ frontend/src/Components/RequestConfirmation.jsx
@@ -3,6 +3,7 @@
 <<<<<<< Updated upstream
 <<<<<<< Updated upstream
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
 
 // src/components/AgriLend/RequestConfirmation.jsx
 import React from 'react';
@@ -32,6 +33,8 @@ const RequestConfirmation = ({ isSale = false, onDone }) => {
 >>>>>>> Stashed changes
 =======
 >>>>>>> Stashed changes
+=======
+>>>>>>> Stashed changes
 import React from 'react';
 import '../styles/AgriLendPage.css';
 import checkmarkImage from '../assets/checkmark.png'; // Use the checkmark image you used in your design
@@ -49,6 +52,9 @@ const RequestConfirmation = () => {
 <<<<<<< Updated upstream
 <<<<<<< Updated upstream
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
+>>>>>>> Stashed changes
+=======
 >>>>>>> Stashed changes
 =======
 >>>>>>> Stashed changes
